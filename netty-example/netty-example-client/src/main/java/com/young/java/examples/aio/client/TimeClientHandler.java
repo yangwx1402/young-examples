bd21606bb4509a9f0d3256c919c5ef9d9cef1817
yangyong <398901650@qq.com>
2016-03-24 21:21:16 +0800

    client代码提交哦啊哦

---
+++ netty-example/netty-example-client/src/main/java/com/young/java/examples/aio/client/TimeClientHandler.java
@@ -0,0 +1,116 @@
+package com.young.java.examples.aio.client;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by issuser on 2016/3/23.
+ */
+public class TimeClientHandler implements Runnable {
+
+    private String host;
+
+    private int port;
+
+    private Selector selector;
+
+    private SocketChannel socketChannel;
+
+    private volatile boolean stop;
+
+    public TimeClientHandler(String host, int port) {
+        this.host = host;
+        this.port = port;
+        try {
+            selector = Selector.open();
+            socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        try {
+            doConnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        while (!stop) {
+            try {
+                selector.select(1000);
+                Set<SelectionKey> selectionKeySet = selector.selectedKeys();
+                Iterator<SelectionKey> it = selectionKeySet.iterator();
+                SelectionKey key = null;
+                while (it.hasNext()) {
+                    key = it.next();
+                    it.remove();
+                    try {
+                        handInput(key);
+                    } catch (Exception e) {
+                        if (key != null) {
+                            key.cancel();
+                            if (key.channel() != null) {
+                                key.channel().close();
+                            }
+                        }
+                        e.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handInput(SelectionKey key) throws IOException {
+        if (key.isValid()) {
+            SocketChannel socketChannel = (SocketChannel) key.channel();
+            if (key.isConnectable()) {
+                socketChannel.register(selector, SelectionKey.OP_READ);
+                doWrite(socketChannel);
+            }
+            if (key.isReadable()) {
+                ByteBuffer readBuffer = ByteBuffer.allocate(1024);
+                int readBytes = socketChannel.read(readBuffer);
+                if (readBytes > 0) {
+                    readBuffer.flip();
+                    byte[] bytes = new byte[readBuffer.remaining()];
+                    readBuffer.get(bytes);
+                    String body = new String(bytes, "UTF-8");
+                    System.out.println("---body is  --" + body);
+                    this.stop = true;
+                } else if (readBytes < 0) {
+                    key.cancel();
+                    socketChannel.close();
+                } else ;
+            }
+        }
+    }
+
+    private void doWrite(SocketChannel socketChannel) throws IOException {
+        byte[] req = "QUERY time order ".getBytes();
+        ByteBuffer writeBuffer = ByteBuffer.allocate(req.length);
+        writeBuffer.put(req);
+        writeBuffer.flip();
+        socketChannel.write(writeBuffer);
+        if(!writeBuffer.hasRemaining()){
+            System.out.println("客户端把数据写完了");
+        }
+    }
+
+    private void doConnect() throws IOException {
+        if (socketChannel.connect(new InetSocketAddress(host, port))) {
+            socketChannel.register(selector, SelectionKey.OP_READ);
+            doWrite(socketChannel);
+        }else{
+            socketChannel.register(selector,SelectionKey.OP_CONNECT);
+        }
+    }
+}
