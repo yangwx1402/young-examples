e0b355db01d99218f4aae1cdb69b67e9f5f74547
yangyong <398901650@qq.com>
2016-07-19 23:19:15 +0800

    hadoop writable

---
+++ hadoop-example/src/main/java/com/young/java/examples/writable/UserWritable.java
@@ -0,0 +1,48 @@
+package com.young.java.examples.writable;
+
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2016/7/19.
+ */
+public class UserWritable implements WritableComparable<UserWritable> {
+
+    private String username;
+
+    private Integer age;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setValue(String username,Integer age){
+        this.username = username;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(UserWritable o) {
+        return o.getUsername().compareTo(username);
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+        dataOutput.writeUTF(username);
+        dataOutput.writeInt(age);
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+        this.username = dataInput.readUTF();
+        this.age = dataInput.readInt();
+    }
+}
