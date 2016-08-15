ec709f220f71997d9ff423b3d7b0a5228bd0f5d4
yangyong <398901650@qq.com>
2016-08-15 16:57:21 +0800

    修改了类加载过程中的bug

--- beans-example/beans-interface-example/src/main/java/com/young/java/examples/classloader/hotswap/HotSwapAction.java
+++ beans-example/beans-interface-example/src/main/java/com/young/java/examples/classloader/hotswap/HotSwapAction.java
@@ -1,8 +1,10 @@
 package com.young.java.examples.classloader.hotswap;
 
+import java.io.IOException;
+
 /**
  * Created by Administrator on 2016/8/14.
  */
 public interface HotSwapAction {
-    public void sayHello();
+    public void sayHello() throws IOException;
 }
--- beans-example/beans-support-example/src/main/java/com/young/java/examples/hotswap/HotSwapActionSupport.java
+++ beans-example/beans-support-example/src/main/java/com/young/java/examples/hotswap/HotSwapActionSupport.java
@@ -1,13 +1,19 @@
 package com.young.java.examples.hotswap;
 
 import com.young.java.examples.classloader.hotswap.HotSwapAction;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.Job;
+
+import java.io.IOException;
 
 /**
  * Created by Administrator on 2016/8/14.
  */
 public class HotSwapActionSupport implements HotSwapAction {
     @Override
-    public void sayHello() {
+    public void sayHello() throws IOException {
         System.out.println("hello world (version 6)");
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf);
     }
 }
